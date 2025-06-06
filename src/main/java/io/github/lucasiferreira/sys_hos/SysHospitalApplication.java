2fe0afc62cc1d4654163ff32ff9126ac7ab93cce
Lucas <lucas.iferreir@gmail.com>
2025-06-06 13:16:32 -0300

    First commit

---
+++ src/main/java/io/github/lucasiferreira/sys_hos/SysHospitalApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasiferreira.sys_hos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lucasiferreira/sys_hos/SysHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasiferreira.sys_hos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
